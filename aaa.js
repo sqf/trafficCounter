3078d0fcd91d81dc8cc553ab5a65f857ea21f8c0
Samir Senhadri <sqf@o2.pl>
2017-06-03 15:38:53 +0200

    Added calibration

--- aaa.js
+++ aaa.js
@@ -1,31 +1,70 @@
 //var exec = require('exec');
 const execSync = require('child_process').execSync;
-console.log("aaaajanusz");
+var program = require('commander');
+var datetime = new Date();
 
-var signalStrengthWithoutNoise = execSync('./wifi-scan-station wlo1');
+program
+    .version('0.0.1')
+    .option('-k, --calibrate', 'Run when there are no vehicles. It will take a few seconds to proceed.')
+    .option('-c, --count', 'Count vehicles')
+    .option('-d, --countAndDebug', 'Count vehicles and save measurements to a file')
+    //.option('-c, --cheese [type]', 'Add the specified type of cheese [marble]', 'marble')
+    .parse(process.argv);
 
-console.log("signalStrengthWithoutNoise: ", signalStrengthWithoutNoise.toString());
+if (program.calibrate) calibrate();
+if (program.count) count();
+if (program.countAndDebug) countAndDebug();
 
-var counter = 0;
+function getCurrentSignalStrength() {
+    return parseFloat(execSync('./wifi-scan-station wlxf81a671a3127').toString());
+}
 
-var isVehiclePassing = false;
-
-setInterval(function() 
+function calibrate ()
 {
-	var currentSignalStrength = execSync('./wifi-scan-station wlo1');
+	console.log("Calibration...");
+	var noVehicleValues = [];
+	var fetchNoVehicleValues = setInterval(function () {
+        var currentSignalStrength = getCurrentSignalStrength();
+        console.log("currentSignalStrength", currentSignalStrength);
+        noVehicleValues.push(currentSignalStrength)
+    }, 50);
 
-	console.log("aaaa currentSignalStrength", currentSignalStrength.toString());
+	setTimeout(function () {
+        clearInterval(fetchNoVehicleValues);
+        var sum = noVehicleValues.reduce(function(a, b) { return a + b; });
+        var avg = (sum / noVehicleValues.length).toFixed();
+        console.log("Calibration done. Average = ", avg);
+        return avg
+    }, 2000)
 
-	if(currentSignalStrength > signalStrengthWithoutNoise )
-	{
-		isVehiclePassing = true;
-		console.log("Low signal strength!!!");
-	}
-	if (currentSignalStrength <= signalStrengthWithoutNoise && isVehiclePassing === true) 
-	{
-		console.log("counter++");
-		counter++;
-		isVehiclePassing = false;
-	}
-}, 1000);
+}
 
+function count() {
+    var counter = 0;
+
+    var isVehiclePassing = false;
+
+    setInterval(function()
+    {
+        var currentSignalStrength = getCurrentSignalStrength();
+
+        //console.log("aaaa currentSignalStrength", currentSignalStrength.toString());
+
+        if(currentSignalStrength > signalStrengthWithoutNoise )
+        {
+            isVehiclePassing = true;
+            //console.log("Low signal strength!!!");
+        }
+        if (currentSignalStrength <= signalStrengthWithoutNoise && isVehiclePassing === true)
+        {
+            //console.log("counter++");
+            counter++;
+            isVehiclePassing = false;
+        }
+    }, 50);
+
+    setInterval(function ()
+    {
+        console.log("counter: ", counter);
+    }, 500)
+}
