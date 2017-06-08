5eaf13cace2e763aff9b05667a800b90651f22ec
Samir Senhadri <sqf@o2.pl>
2017-06-08 06:55:13 +0200

    Now user can put his observation to the program and it will be saved to a file.

--- aaa.js
+++ aaa.js
@@ -2,8 +2,13 @@
 const execSync = require('child_process').execSync;
 var fs = require('fs');
 var program = require('commander');
+const readline = require('readline');
+readline.emitKeypressEvents(process.stdin);
+process.stdin.setRawMode(true);
+
 var datetime = new Date();
 
+var threshold = 5;
 program
     .version('0.0.1')
     .option('-k, --calibrate', 'Run when there are no vehicles. It will take a few seconds to proceed.')
@@ -17,7 +22,7 @@ if (program.count) count();
 if (program.countAndDebug) countAndDebug();
 
 function getCurrentSignalStrength() {
-    return parseFloat(execSync('./wifi-scan-station wlxf81a671a3127').toString());
+    return parseFloat(execSync('./wifi-scan-station wlo1').toString());i
 }
 
 function calibrate ()
@@ -39,7 +44,6 @@ function calibrate ()
             if(err) {
                 return console.log(err);
             }
-
             console.log("Results saved to a file.");
         });
         return avg
@@ -48,12 +52,19 @@ function calibrate ()
 }
 
 function count() {
+    console.log("Press <q> to quit.");
+    console.log("Press <c> if you detected a car.");
+    console.log("Press <t> if you detected a truck.");
+    console.log("Press <b> if you detected a bus.");
+    console.log("Press <r> if you detected a bicycle.");
+    console.log("Press <m> if you detected a motorcycle.");
     var counter = 0;
-
     var isVehiclePassing = false;
+    
     var signalStrengthWithoutNoise = parseFloat(fs.readFileSync("calibrationResult").toString());
     var t = new Date();
     var filePathAndName = "results/" + t.toDateString() + " " + t.getUTCHours() + " " + t.getUTCMinutes() + " " + t.getUTCSeconds();
+    var filePathAndNameDebug = filePathAndName + " DEBUG";
     console.log("Average no vehicle signal strength is: ", signalStrengthWithoutNoise);
     fs.writeFile(filePathAndName,
         "Counting started at " + t + "\nCalibration value is " + signalStrengthWithoutNoise + "dBm", function(err) {
@@ -63,37 +74,62 @@ function count() {
         });
 
 
-    fs.writeFile(filePathAndName + " DEBUG",
+    fs.writeFile(filePathAndNameDebug,
         "Counting started at " + t + "\nCalibration value is " + signalStrengthWithoutNoise + "dBm", function(err) {
             if(err) {
                 return console.log(err);
             }
         });
+
+    function logThatUserDetected(vehicle) {
+        console.log("You detected a " + vehicle + "!");
+        var tNow = new Date();
+        fs.appendFileSync(filePathAndName, "\n" + tNow + tNow.getUTCMilliseconds() + " User detected a " + vehicle + "!");
+        fs.appendFileSync(filePathAndNameDebug, "\n" + tNow + tNow.getUTCMilliseconds() + " User detected a " + vehicle +"!");
+    }
+
+    process.stdin.on('keypress', (str, key) => {
+        switch(key.sequence) {
+            case "q":
+                process.exit();
+            case "c":
+                logThatUserDetected("car");
+                break;
+            case "t":
+                logThatUserDetected("truck");
+                break;
+            case "b":
+                logThatUserDetected("bus");
+                break;
+            case "r":
+                logThatUserDetected("bicycle");
+                break;
+            case "m":
+                logThatUserDetected("motorcycle");
+                break;
+        }
+    });
     setInterval(function()
     {
         var tNow = new Date();
         var currentSignalStrength = getCurrentSignalStrength();
 
-        console.log("aaaa currentSignalStrength", currentSignalStrength.toString());
-        fs.appendFileSync(filePathAndName + " DEBUG", "\n" + tNow + tNow.getUTCMilliseconds() + " " + currentSignalStrength);
+        //console.log("aaaa currentSignalStrength", currentSignalStrength.toString());
+        fs.appendFileSync(filePathAndNameDebug, "\n" + tNow + tNow.getUTCMilliseconds() + " " + currentSignalStrength);
 
-        if(currentSignalStrength > signalStrengthWithoutNoise - 5)
+        if(currentSignalStrength > signalStrengthWithoutNoise - threshold)
         {
             isVehiclePassing = true;
             //console.log("Low signal strength!!!");
         }
-        if (currentSignalStrength <= signalStrengthWithoutNoise - 5 && isVehiclePassing === true)
+        if (currentSignalStrength <= signalStrengthWithoutNoise - threshold && isVehiclePassing === true)
         {
             //console.log("counter++");
             counter++;
-            fs.appendFileSync(filePathAndName, "\n" + tNow + " Vehicle detected !");
-            fs.appendFileSync(filePathAndName + " DEBUG", "\n" + tNow + tNow.getUTCMilliseconds() + " Vehicle detected !");
+            fs.appendFileSync(filePathAndName, "\n" + tNow + " Vehicle detected!");
+            fs.appendFileSync(filePathAndNameDebug, "\n" + tNow + tNow.getUTCMilliseconds() + " Vehicle detected!");
             isVehiclePassing = false;
+            console.log("counter: ", counter);
         }
-    }, 50);
-
-    setInterval(function ()
-    {
-        console.log("counter: ", counter);
-    }, 500)
+    }, 10);
 }
