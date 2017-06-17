27960e8046de8f0c44abd22ffb9cf5426ed95ad1
Samir Senhadri <sqf@o2.pl>
2017-06-17 20:59:49 +0200

    Added possibility to spot fast vehicles. Refactoring

--- aaa.js
+++ aaa.js
@@ -9,6 +9,7 @@ process.stdin.setRawMode(true);
 var datetime = new Date();
 
 var threshold = 5;
+var apName = "wlo1";
 program
     .version('0.0.1')
     .option('-k, --calibrate', 'Run when there are no vehicles. It will take a few seconds to proceed.')
@@ -22,10 +23,10 @@ if (program.count) count();
 if (program.countAndDebug) countAndDebug();
 
 function getCurrentSignalStrength() {
-    return parseFloat(execSync('./wifi-scan-station wlo1').toString());i
+    return parseFloat(execSync('./wifi-scan-station ' + apName).toString());
 }
 
-function calibrate ()
+function calibrate()
 {
 	console.log("Calibration...");
 	var noVehicleValues = [];
@@ -35,7 +36,7 @@ function calibrate ()
         noVehicleValues.push(currentSignalStrength)
     }, 50);
 
-	setTimeout(function () {
+	setTimeout(function() {
         clearInterval(fetchNoVehicleValues);
         var sum = noVehicleValues.reduce(function(a, b) { return a + b; });
         var avg = (sum / noVehicleValues.length).toFixed();
@@ -46,7 +47,7 @@ function calibrate ()
             }
             console.log("Results saved to a file.");
         });
-        return avg
+        return avg;
     }, 2000)
 
 }
@@ -54,10 +55,16 @@ function calibrate ()
 function count() {
     console.log("Press <q> to quit.");
     console.log("Press <c> if you detected a car.");
+    console.log("Press <d> if you detected a fast car.");
     console.log("Press <t> if you detected a truck.");
+    console.log("Press <5> if you detected a fast truck.");
     console.log("Press <b> if you detected a bus.");
+    console.log("Press <g> if you detected a fast bus.");
     console.log("Press <r> if you detected a bicycle.");
+    console.log("Press <4> if you detected a fast bicycle.");
     console.log("Press <m> if you detected a motorcycle.");
+    console.log("Press <j> if you detected a fast motorcycle.");
+    console.log("Fast means more than 40 kph.");
     var counter = 0;
     var isVehiclePassing = false;
     
@@ -95,17 +102,32 @@ function count() {
             case "c":
                 logThatUserDetected("car");
                 break;
+            case "d":
+                logThatUserDetected("fast car");
+                break;
             case "t":
                 logThatUserDetected("truck");
+                break;
+            case "5":
+                logThatUserDetected("fast truck");
                 break;
             case "b":
                 logThatUserDetected("bus");
                 break;
+            case "g":
+                logThatUserDetected("fast bus");
+                break;
             case "r":
                 logThatUserDetected("bicycle");
                 break;
+            case "4":
+                logThatUserDetected("fast bicycle");
+                break;
             case "m":
                 logThatUserDetected("motorcycle");
+                break;
+            case "j":
+                logThatUserDetected("fast motorcycle");
                 break;
         }
     });
