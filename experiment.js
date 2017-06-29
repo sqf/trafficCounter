0c5517d73fd6b825031b1817f89cee4bbc174e54
Samir Senhadri <sqf@o2.pl>
2017-06-29 11:42:18 +0200

    Added executable program that is needed to get current signal strength (this is a modification of example from wifi-scan library). Added package.json. Added draft of 'experiment' feature.

---
+++ experiment.js
@@ -0,0 +1,8 @@
+var td = require('testdouble');
+var trafficCounter = require("../trafficCounter.js");
+var utils = require("../utils.js");
+
+var getCurrentSignalStrength = td.replace(utils, "getCurrentSignalStrength");
+td.when(getCurrentSignalStrength("wlxf81a671a3127")).thenReturn("-65", "-75", "-65");
+
+trafficCounter.count();
